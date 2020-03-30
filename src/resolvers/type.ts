b48294599b86177812ea5b56ff87408d3d7b4863
Bikcode <bikcodeh@gmail.com>
2020-03-30 16:36:05 -0500

    chore: configura query para obtener las carreras por anio

--- src/resolvers/type.ts
+++ src/resolvers/type.ts
@@ -1,11 +1,22 @@
 import { IResolvers } from "graphql-tools";
-import { LIST } from "./resolversMap";
 import { getWikipediaMobileUrl } from "../lib/utils";
 
 const type: IResolvers = {
     Season: {
         year: parent => parent.season,
-        urlMobile: parent => getWikipediaMobileUrl(parent.url)
+        urlMobile: parent => getWikipediaMobileUrl(parent.url),
+    },
+    Race: {
+        name: parent => parent.raceName,
+        circuit: parent => parent.Circuit
+    },
+    Circuit: {
+        id: parent => parent.circuitId,
+        name: parent => parent.circuitName,
+        location: parent => parent.Location
+    },
+    Location: {
+        lng: parent => parent.long
     }
 }
 
