004621ea3b0a0123006980314e36d3d6d72e9724
Bikcode <bikcodeh@gmail.com>
2020-03-31 22:47:34 -0500

    feature: agrega query para obtener piloto por id

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -2,36 +2,41 @@ import { IResolvers } from "graphql-tools";
 
 const query: IResolvers = {
     Query: {
-        async seasonList(_: void, __: any, { dataSources } ) {
+        async seasonList(_: void, __: any, { dataSources }) {
             return await dataSources.seasons.getSeasons().then(
                 (data: any) => data.MRData.SeasonTable.Seasons
             );
         },
-         async racesByYear(_: void, { year }, { dataSources } ){
+        async racesByYear(_: void, { year }, { dataSources }) {
             return await dataSources.races.getYear(year).then(
                 (data: any) => data.MRData.RaceTable.Races
             );
-         },
-         async raceSelect(_: void, { year, round }, { dataSources }){
+        },
+        async raceSelect(_: void, { year, round }, { dataSources }) {
             return await dataSources.races.getYearRound(year, round).then(
                 (data: any) => data.MRData.RaceTable.Races[0]
             );
-         },
-         async historyDrivers(_: void, { pageElements, page }, { dataSources }){
+        },
+        async historyDrivers(_: void, { pageElements, page }, { dataSources }) {
             return await dataSources.drivers.getDrivers(pageElements, page).then(
                 (data: any) => data.MRData.DriverTable.Drivers
             );
-         },
-         async driversYear(_: void, { year }, { dataSources }){
+        },
+        async driversYear(_: void, { year }, { dataSources }) {
             return await dataSources.drivers.getDriversByYear(year).then(
                 (data: any) => data.MRData.DriverTable.Drivers
             );
-         },
-         async driversYearAndRound(_: void, { year, round }, { dataSources }){
+        },
+        async driversYearAndRound(_: void, { year, round }, { dataSources }) {
             return await dataSources.drivers.getDriversByYearAndRound(year, round).then(
                 (data: any) => data.MRData.DriverTable.Drivers
             );
-         }
+        },
+        async driverById(_: void, { id }, { dataSources }){
+            return await dataSources.drivers.getDriverById(id).then(
+                (data: any) => data.MRData.DriverTable.Drivers[0]
+            );
+        }
 
     }
 }
